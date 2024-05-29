83a46657c5ef587ad5ac0fd33ca41386dfab252b
Kedar Singh <kedar.singh@decentro.tech>
2024-05-30 02:38:55 +0530

    updated

--- app/_init_.py
+++ app/_init_.py
@@ -1,8 +1,11 @@
 from flask import Flask
-
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
+
+    # Allow all origins for CORS
+    CORS(app)
 
     with app.app_context():
         from .controllers import sentiment_bp
--- run.py
+++ run.py
@@ -4,6 +4,5 @@ from app._init_ import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app = create_app()
     port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if PORT is not set
     app.run(host="0.0.0.0", port=port)
